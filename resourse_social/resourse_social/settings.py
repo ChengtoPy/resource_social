aacfdb4bb9570e6bb638daebb5537c60c9ab9a7d
qike <18309610151@163.com>
2019-12-12 08:50:39 +0800

    'feat:配置前端静态文件'

--- resourse_social/resourse_social/settings.py
+++ resourse_social/resourse_social/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# 配置静态文件加载路径
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 CACHES = {
     "default": {  # 默认
         "BACKEND": "django_redis.cache.RedisCache",
