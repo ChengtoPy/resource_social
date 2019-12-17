458b595f5d2df960d63bc652c986a5d4ff2d5c20
qike <18309610151@163.com>
2019-12-17 10:43:33 +0800

    'feat:展示精品资源页面'

--- resourse_social/apps/srik/urls.py
+++ resourse_social/apps/srik/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from .views import IndexView
-
+from .views import IndexView, GoodClass
 
 app_name='srik'
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^goodclass/$', GoodClass.as_view(), name="goodclass"),
 ]
--- resourse_social/apps/srik/views.py
+++ resourse_social/apps/srik/views.py
@@ -23,4 +23,36 @@ class IndexView(View):
 
         return render(request, 'blackmain/index.html', {'contacts': contacts, 'paginator': paginator})
 
+
+class GoodClass(View):
+    """精品教程"""
+    def get(self,request):
+        contact_list = Posts.objects.all().order_by("-create_time")
+        paginator = Paginator(contact_list, 12)  # Show 25 contacts per page
+
+        page = request.GET.get('page', '1')
+        try:
+            contacts = paginator.page(page)
+        except PageNotAnInteger:
+            # 如果页面不是整数，则提交第一个页面
+            contacts = paginator.page(1)
+        except EmptyPage:
+            #  如果页面超出范围，提交最后一个页面
+            contacts = paginator.page(paginator.num_pages)
+        list2 = []
+        list3 = []
+        if len(contacts)>=4 and len(contacts)<8:
+            list1=[contacts[0],contacts[1],contacts[2],contacts[3]]
+        elif len(contacts)>=8 and len(contacts)<12:
+            list1 = [contacts[0], contacts[1], contacts[2], contacts[3]]
+            list2 = [contacts[4], contacts[5], contacts[6], contacts[7]]
+        elif len(contacts)>=12:
+            list1 = [contacts[0], contacts[1], contacts[2], contacts[3]]
+            list2 = [contacts[4], contacts[5], contacts[6], contacts[7]]
+            list3 = [contacts[8], contacts[9], contacts[10], contacts[11]]
+        else:
+            list1 = [contacts[0], contacts[1], contacts[2], contacts[3]]
+        print(len(list1))
+        return render(request, 'blackmain/goodclass.html', {'contacts': contacts, 'paginator': paginator,'list1':list1,'list2':list2,'list3':list3})
+
 # Create your views here.
