4e0e50891ac094f8c5a0ca38b8f92e809da57107
qike <18309610151@163.com>
2019-12-18 20:32:44 +0800

    'feat:添加用户问题提交功能'

--- resourse_social/apps/srik/urls.py
+++ resourse_social/apps/srik/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
-from .views import IndexView, GoodClass, EnjoyView, BwView, BcView, AnswerView, GetView, VipView, CommentView
+from .views import IndexView, GoodClass, EnjoyView, BwView, BcView, AnswerView, GetView, VipView, CommentView, \
+    QuestionView
 
 app_name='srik'
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
     url(r'^getcoin/$',GetView.as_view(),name='get'),
     url(r'^vip/$',VipView.as_view(),name='vip'),
     url(r'^comment/$',CommentView.as_view(),name='comment'),
+    url(r'^question/$',QuestionView.as_view(),name='question'),
 ]
