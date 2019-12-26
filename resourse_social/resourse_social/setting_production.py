d3614224d2a2d0d2d77acf4406117f7bf645537c
qike <18309610151@163.com>
2019-12-26 09:36:44 +0800

    'build:构建uwsgi文件，以及配置开发环境和生产环境'

--- resourse_social/resourse_social/setting_production.py
+++ resourse_social/resourse_social/setting_production.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-vpafmo7t0=^6g%!epj!%p1l((d#q0h3x90wi*#m&l+s0ouzbx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
